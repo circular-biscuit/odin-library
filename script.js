525782fae734d390bd5edceb57147b933a175e73
circular-biscuit <mrdeb99@gmail.com>
2022-02-03 14:18:00 +0530

    formatted form

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ function Book(title, author, pages, read){
 }
 
 //book info display function added to prototype
-Book.prototype.info = function(){
-    if (Book.read){
-        return (Book.title + ' by ' + Book.author + ', ' + Book.pages + ' pages, yes I have read it.')
-    } else { 
-        return (Book.title + ' by ' + Book.author + ', ' + Book.pages + ' pages, no i have not read it.')
-    }
-}
+// Book.prototype.info = function(){
+//     if (Book.read){
+//         return (Book.title + ' by ' + Book.author + ', ' + Book.pages + ' pages, yes I have read it.')
+//     } else { 
+//         return (Book.title + ' by ' + Book.author + ', ' + Book.pages + ' pages, no i have not read it.')
+//     }
+// }
 
 //add user input book into library array
-function addBook(){
+function addBook(e){
+    e.preventDefault();
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author');
     let pages = document.querySelector('#pages');
-    const newBook = new Book(title, author, pages, read)
-    myLibrary.push(newBook)
-    console.log(myLibrary)
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
 
 //get submit button
 const submitBook = document.querySelector('.submit-button');
 //add book to array on button click
-submitBook.addEventListener('click', addBook);
-
+submitBook.addEventListener('click', (e) => addBook);
